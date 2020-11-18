538dc163ce41ef798d342121b39df79f37a5ea2b
Alexandru Oriean <alexandru.oriean@gmail.com>
2020-11-18 20:28:13 +0200

    Styled register page

--- src/components/registration.js
+++ src/components/registration.js
@@ -25,41 +25,51 @@ export default function Register() {
       <form onSubmit={(e) => e.preventDefault}>
         <div className="form-group row">
           <label className="col-sm-2 col-form-label">Name: </label>
-          <input
-            type="text"
-            name="name"
-            placeholder="Name"
-            ref={register({ required: true })}
-          />
+          <div className="col-sm-10">
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              ref={register({ required: true })}
+            />
+          </div>
         </div>
-
         <div className="form-group row">
           <label className="col-sm-2 col-form-label">Username: </label>
-          <input
-            type="text"
-            name="username"
-            placeholder="User name"
-            ref={register({ required: true })}
-          />
+          <div className="col-sm-10">
+            <input
+              type="text"
+              name="username"
+              placeholder="User name"
+              ref={register({ required: true })}
+            />
+          </div>
         </div>
 
         <div className="form-group row">
           <label className="col-sm-2 col-form-label">Email: </label>
-          <input
-            type="email"
-            name="email"
-            placeholder="name@domain.com"
-            ref={register({ required: true })}
-          />
+          <div className="col-sm-10">
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              id="inputEmail3"
+              placeholder="name@domain.com"
+              ref={register({ required: true })}
+            />
+          </div>
         </div>
 
         <div className="form-group row">
           <label className="col-sm-2 col-form-label">Password: </label>
-          <input
-            type="password"
-            name="password"
-            ref={register({ required: true })}
-          />
+          <div className="col-sm-10">
+            <input
+              type="password"
+              name="password"
+              className="col-sm-2 col-form-label"
+              ref={register({ required: true })}
+            />
+          </div>
         </div>
 
         <div className="form-group row">
@@ -86,11 +96,11 @@ export default function Register() {
 
       {/* <form>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+      <input type="email"  placeholder="Email">
     </div>
   </div>
   <div class="form-group row">
-    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+    <label for="inputPassword3" >Password</label>
     <div class="col-sm-10">
       <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
     </div>
